4ec3bda95abfb108ef50bea8779bc8df4fcce85b
julesmichaud <jules.michaud@yahoo.fr>
2020-05-26 10:24:37 +0200

    salut a tous

--- test_jules.py
+++
@@ -1,3 +0,0 @@
-__author__ = 'julesmichaud'
-__filename__ = 'test_jules.py'
-__date__ = '26/05/20'
---
+++ tests_jules.py
@@ -0,0 +1,7 @@
+__author__ = 'julesmichaud'
+__filename__ = 'tests_jules.py'
+__date__ = '26/05/20'
+
+import numpy as np
+import matplotlib.pyplot as plt
+
